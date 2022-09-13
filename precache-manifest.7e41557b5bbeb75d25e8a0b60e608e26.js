6278d0262bebbccf6a3240f66ab49f060574f28c
asterism0115 <goldstar11520@gmail.com>
2022-09-13 16:47:57 +0800

    Updates

---
+++ precache-manifest.7e41557b5bbeb75d25e8a0b60e608e26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecce137a280217e65fd31cfe5b1baa9d",
+    "url": "/Frontend-Task/index.html"
+  },
+  {
+    "revision": "b091e6ef144a02d4f8c0",
+    "url": "/Frontend-Task/static/css/main.8a2f77bd.chunk.css"
+  },
+  {
+    "revision": "2e2daccf661a4afe9240",
+    "url": "/Frontend-Task/static/js/2.d8afe540.chunk.js"
+  },
+  {
+    "revision": "b091e6ef144a02d4f8c0",
+    "url": "/Frontend-Task/static/js/main.498f7ed0.chunk.js"
+  },
+  {
+    "revision": "8ce135eaad54711a1935",
+    "url": "/Frontend-Task/static/js/runtime~main.343fb1b3.js"
+  }
+]);
